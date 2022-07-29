c10ac879589259d058fcdb61f885dfc1a2a5db36
xya499 <302870488@qq.com>
2022-07-29 19:46:27 +0800

    完成表单相关页面

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,12 +1,111 @@
 <template>
-  <div>editor</div>
+  <div class="editor-box">
+    <!-- 头部 -->
+    <div class="editor-title">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <div class="editor-box">
+            <el-icon>
+              <List />
+            </el-icon>
+            <span>富文本编辑器</span>
+          </div>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+
+    </div>
+    <div class="editor-container">
+      <div style="border: 1px solid #ccc" class="editor">
+        <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig"
+          :mode="mode" />
+        <Editor style="height: 500px; overflow-y: hidden;" v-model="valueHtml" :defaultConfig="editorConfig"
+          :mode="mode" @onCreated="handleCreated" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import '@wangeditor/editor/dist/css/style.css' // 引入 css
 
+import { onBeforeUnmount, ref, shallowRef, onMounted } from 'vue'
+import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
+
+export default {
+  components: { Editor, Toolbar },
+  setup() {
+    // 编辑器实例，必须用 shallowRef
+    const editorRef = shallowRef()
+
+    // 内容 HTML
+    const valueHtml = ref('<p>hello</p>')
+
+    // 模拟 ajax 异步获取内容
+    onMounted(() => {
+      setTimeout(() => {
+        valueHtml.value = '<p>模拟 Ajax 异步设置内容</p>'
+      }, 1500)
+    })
+
+    const toolbarConfig = {}
+    const editorConfig = { placeholder: '请输入内容...' }
+
+    // 组件销毁时，也及时销毁编辑器
+    onBeforeUnmount(() => {
+      const editor = editorRef.value
+      if (editor == null) return
+      editor.destroy()
+    })
+
+    const handleCreated = (editor) => {
+      editorRef.value = editor // 记录 editor 实例，重要！
+    }
+
+    return {
+      editorRef,
+      valueHtml,
+      mode: 'default', // 或 'simple'
+      toolbarConfig,
+      editorConfig,
+      handleCreated
+    };
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.editor-box {
+  padding: 10px;
+  ;
+  background-color: #f0f0f0;
+
+  .editor-title {
+    padding: 10px 0;
+    width: 100%;
+    height: 20px;
+
+    .editor-box {
+      display: flex;
+      align-items: center;
+
+      span {
+        font-size: 16px;
+      }
+
+      .el-icon {
+        font-size: 20px;
+      }
+    }
+  }
+
+  .editor-container {
+    width: 100%;
+    background-color: #fff;
+
+    .editor {
+      padding: 10px;
+      margin: 10px;
+    }
+  }
+}
 </style>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>upload</div>
+  <div class="upload-box">
+    <!-- 头部 -->
+    <div class="upload-title">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <div class="upload-box">
+            <el-icon>
+              <List />
+            </el-icon>
+            <span>表格/文件上传</span>
+          </div>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="upload-container">
+      <el-upload class="upload-demo" drag action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        multiple>
+        <el-icon class="el-icon--upload">
+          <upload-filled />
+        </el-icon>
+        <div class="el-upload__text">
+          将文件拖到此处，或<em>点击上传</em>
+        </div>
+        <template #tip>
+          <div class="el-upload__tip">
+            只能上传 jpg/png 文件，且不超过 500kb
+          </div>
+        </template>
+      </el-upload>
+    </div>
+  </div>
 </template>
 
 <script>
+import { UploadFilled } from '@element-plus/icons-vue'
 export default {
 
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
+.upload-box {
+  padding: 10px;
+  ;
+  background-color: #f0f0f0;
+
+  .upload-title {
+    padding: 10px 0;
+    width: 100%;
+    height: 20px;
+
+    .upload-box {
+      display: flex;
+      align-items: center;
+
+      span {
+        font-size: 16px;
+      }
+
+      .el-icon {
+        font-size: 20px;
+      }
+    }
+  }
+
+  .upload-container {
+    width: 100%;
+    background-color: #fff;
+    .upload-demo{
+      padding: 10px;
+      margin: 10px;
+    }
+  }
+}
 </style>
